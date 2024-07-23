2b4810b471f318104c7e0b4deb35d2dce69cc995
nhatthan2k <nhatthan0910@gmail.com>
2024-07-23 09:59:34 +0700

    fig home

--- fontend/src/Pages/Home/Home.js
+++ fontend/src/Pages/Home/Home.js
@@ -5,18 +5,71 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { FilmList } from '../../filmlist/Filmlist';
 import SliderItem from '../../component/SliderItem/SliderItem';
-import NavContent from '../../component/NavContent/NavContent';
 import Content from '../../Layout/component/Content/Content';
 import { useMediaQuery } from 'react-responsive';
 import Header from '../../Layout/component/Header/Header';
 import Navbar from '../../Layout/component/Navbar/Navbar';
 import Footer from '../../Layout/component/Footer/Footer';
+import { useDispatch, useSelector } from 'react-redux';
+import { SEASON } from '../../redux/selectors/selectors';
+import SectionBar from '../../component/SectionBar/SectionBar';
+import MovieItem from '../../component/MovieItem/MovieItem';
 
 const cx = classNames.bind(Styles);
 
+const daylist = [
+    { engDay: 'Mới', VnDay: 'Cập Nhật' },
+    { engDay: 'Mon', VnDay: 'Thứ Hai' },
+    { engDay: 'Tue', VnDay: 'Thứ Ba' },
+    { engDay: 'Wed', VnDay: 'Thứ Tư' },
+    { engDay: 'Thu', VnDay: 'Thứ Năm' },
+    { engDay: 'Fri', VnDay: 'Thứ Sau' },
+    { engDay: 'Sat', VnDay: 'Thứ Bảy' },
+    { engDay: 'Sun', VnDay: 'Chử Nhật' },
+];
+
 function Home() {
+    const dispatch = useDispatch();
+    const seasons = useSelector(SEASON);
     const btnRefs = useRef([]);
+    const navRefs = useRef([]);
     const isPc = useMediaQuery({ maxWidth: 1023 });
+    const isMobile = useMediaQuery({ maxWidth: 739 });
+
+    const [toggleDay, setToggleDay] = useState(true);
+    const [selecday, setSelecday] = useState('');
+    const [active, setActive] = useState(0);
+    const [hiddenFilm, setHiddenFilm] = useState([]);
+    const [hiddeSectionBar, setHiddensectionBar] = useState(false);
+
+    const handleClick = (index) => {
+        setActive(index);
+
+        const spanElement = navRefs.current[index].querySelector('span');
+        setSelecday(spanElement.innerText);
+    };
+
+    useEffect(() => {
+        if (isMobile) {
+            setToggleDay(false);
+        } else setToggleDay(true);
+    }, [isMobile]);
+
+    useEffect(() => {
+        if (selecday === '' || selecday === 'Mới') {
+            setHiddensectionBar(true);
+            setHiddenFilm(FilmList);
+            return;
+        } else {
+            setHiddensectionBar(false);
+        }
+
+        const filterFilm = FilmList.filter((Film) => {
+            return Film.Showdate && Film.Showdate.includes(selecday);
+        });
+
+        setHiddenFilm(filterFilm);
+    }, [selecday]);
 
     const favoriteFilm = FilmList.filter((FilmItem) => {
         return FilmItem.hasOwnProperty('Poster');
@@ -103,7 +156,64 @@ function Home() {
                     </div>
                 </div>
 
-                <NavContent />
+                <div className={cx('navContent')}>
+                    <ul>
+                        {isMobile && (
+                            <li className={cx('toggleNav')}>
+                                <button onClick={() => setToggleDay(!toggleDay)}>Lịch Chiếu</button>
+                            </li>
+                        )}
+                        {toggleDay &&
+                            daylist.map((dayItem, index) => (
+                                <li key={index}>
+                                    <button
+                                        ref={(el) => (navRefs.current[index] = el)}
+                                        onClick={() => handleClick(index)}
+                                        className={active === index ? cx('active') : ''}
+                                    >
+                                        <span>{dayItem.engDay}</span>
+                                        <br />
+                                        {dayItem.VnDay}
+                                    </button>
+                                </li>
+                            ))}
+                    </ul>
+                </div>
+
+                <div className={cx('mainContent')}>
+                    {hiddeSectionBar && <SectionBar>Mới Cập Nhật</SectionBar>}
+
+                    <div className={cx('showMovie')}>
+                        {hiddenFilm.map((FilmItem, index) => (
+                            <MovieItem data={FilmItem} key={index} />
+                        ))}
+                    </div>
+
+                    <div className={cx('numberPage')}>
+                        <ul>
+                            <li>
+                                <span className={cx('current')}>1</span>
+                            </li>
+                            <li>
+                                <span>2</span>
+                            </li>
+                            <li>
+                                <span>3</span>
+                            </li>
+                            <li>
+                                <span className={cx('dots')}>...</span>
+                            </li>
+                            <li>
+                                <span>10</span>
+                            </li>
+                            <li>
+                                <span>
+                                    <FontAwesomeIcon icon={faAngleRight} />
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </Content>
             <Footer />
         </>
--- fontend/src/redux/reducers/seasonSlice.jsx
+++ fontend/src/redux/reducers/seasonSlice.jsx
@@ -1,4 +1,4 @@
-import { GET_ALL_SEASON } from '../api/service/seasonService';
+import { GET_ALL_SEASON, GET_ALL_SEASON_HOME } from '../api/service/seasonService';
 import { createSlice } from '@reduxjs/toolkit';
 
 const seasonSlice = createSlice({
@@ -30,6 +30,16 @@ const seasonSlice = createSlice({
                 state.status = 'pending';
             })
             .addCase(GET_ALL_SEASON.fulfilled, (state, action) => {
+                state.status = '';
+                state.seasons = action.payload.content;
+                state.totalPages = action.payload.totalPages;
+                state.size = action.payload.size;
+                state.current = action.payload.number + 1;
+            })
+            .addCase(GET_ALL_SEASON_HOME.pending, (state) => {
+                state.status = 'pending';
+            })
+            .addCase(GET_ALL_SEASON_HOME.fulfilled, (state, action) => {
                 state.status = '';
                 state.seasons = action.payload.content;
                 state.totalPages = action.payload.totalPages;
